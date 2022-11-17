080cc4bf88c52bb5113aad9269bf3423f77e40cb
DerBot7214 <lengwenath1@gmail.com>
2022-11-17 17:08:32 +0100

    activities

--- components/ActivityChart.tsx
+++ components/ActivityChart.tsx
@@ -17,9 +17,13 @@ export default function ActivityChart({ chartData }: Props) {
   const total = Object.values(chartData).reduce((acc, cur) => acc + Object.values(cur)[0], 0)
 
   const rectSize = 12
+  const rectSpacing = 4
 
   const weeks = []
   let days = []
+
+  if (first.weekday() !== 1)
+    for (let i = 0; i < first.weekday(); i++) days.push(<rect key={i} width={rectSize} height={rectSize} fill='transparent' />)
 
   for (let i = 0; i < 365; i++) {
     const date = first.add(1, 'day').format('YYYY-MM-DD')
@@ -29,7 +33,7 @@ export default function ActivityChart({ chartData }: Props) {
       <rect
         key={date}
         x={0}
-        y={(i % 7) * (rectSize + 2)}
+        y={(first.weekday() === 0 ? 6 : first.weekday() - 1) * (rectSize + rectSpacing)}
         rx={2}
         width={rectSize}
         height={rectSize}
@@ -40,7 +44,7 @@ export default function ActivityChart({ chartData }: Props) {
 
     if (days.length === 7) {
       weeks.push(
-        <g key={date} transform={`translate(${weeks.length * (rectSize + 2)}, 0)`}>
+        <g key={date} transform={`translate(${weeks.length * (rectSize + rectSpacing)}, 0)`}>
           {days}
         </g>
       )
@@ -49,17 +53,25 @@ export default function ActivityChart({ chartData }: Props) {
   }
 
   weeks.push(
-    <g key={weeks.length} transform={`translate(${weeks.length * (rectSize + 2)}, 0)`}>
+    <g key={weeks.length} transform={`translate(${weeks.length * (rectSize + rectSpacing)}, 0)`}>
       {days}
     </g>
   )
 
   return (
-    <div className=''>
+    <div>
       <h1 className='pb-1 tracking-wide'><span className='font-semibold'>{total} activites</span> in the last year</h1>
-      <svg width={(rectSize + 2) * 53} height={(rectSize + 2) * 7}>
-        {weeks}
-      </svg>
+      <div className='flex flex-row space-x-3'>
+        <div className='relative flex flex-col items-end font-thin text-sm' style={{ fontSize: `${(rectSpacing / 2) + rectSize}px` }}>
+          <span className={`mt-[-${rectSpacing}px]`}>Mon</span>
+          <span className='absolute' style={{ top: `${((rectSpacing + rectSize) * 2) - rectSpacing}px` }}>Wed</span>
+          <span className='absolute' style={{ top: `${((rectSpacing + rectSize) * 4) - rectSpacing}px` }}>Fri</span>
+          <span className='absolute' style={{ top: `${((rectSpacing + rectSize) * 6) - rectSpacing}px` }}>Sun</span>
+        </div>
+        <svg width={(rectSize + rectSpacing) * 53} height={(rectSize + rectSpacing) * 7}>
+          {weeks}
+        </svg>
+      </div>
     </div>
   )
 }
