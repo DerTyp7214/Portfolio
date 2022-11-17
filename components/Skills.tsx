d0c4b5ddd39fafe9191f4b15bd342f1ec52e30e8
DerBot7214 <lengwenath1@gmail.com>
2022-11-17 16:31:43 +0100

    fixed animation

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -32,8 +32,11 @@ export default function Skills({ skills, chartData, badges }: Props) {
                     <div className='opacity-0 w-0 lg:opacity-100 lg:w-auto xl:opacity-0 xl:w-0 grid grid-cols-4 gap-5'>
                         {skills?.slice(0, Math.min(skills.length, 4 * 5))?.map((skill, index) => <SkillItem key={index} skill={skill} directionLeft={index % 4 < 2} />)}
                     </div>
-                    <div className='w-0 opacity-0 xl:opacity-100 xl:w-auto grid grid-cols-5 gap-5'>
+                    <div className='w-0 opacity-0 xl:opacity-100 xl:w-auto 2xl:opacity-0 2xl:w-0 grid grid-cols-5 gap-5'>
                         {skills?.slice(0, Math.min(skills.length, 5 * 5))?.map((skill, index) => <SkillItem key={index} skill={skill} directionLeft={index % 5 < (index > skills.length / 2 ? 3 : 2)} />)}
+                    </div>
+                    <div className='w-0 opacity-0 2xl:opacity-100 2xl:w-auto grid grid-cols-5 gap-5'>
+                        {skills?.slice(0, Math.min(skills.length, 5 * 5))?.map((skill, index) => <SkillItem key={index} skill={skill} directionLeft />)}
                     </div>
                 </div>
                 <div className='pt-12 flex flex-col items-center space-y-10 opacity-0 w-0 2xl:opacity-100 2xl:w-auto'>
